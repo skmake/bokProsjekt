f9e2dfcc5fa0a5824f4304e00b7ac376e4c442d4
as-troska <troska@asvg.no>
2023-03-14 10:06:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const session = require("express-session");
+const db = require("better-sqlite3")("db.db", {verbose: console.log})
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname + "/public")))
+app.use(express.urlencoded({extended: true}))
+
+
+
+app.get("/", (req,res) => {
+    res.redirect("index.html")
+})
+
+
+app.get("/forfattere", (req, res) => {
+    res.send(db.prepare("SELECT * FROM author").all())
+})
+
+app.get("/boker", (req, res) => {
+    res.send(db.prepare("SELECT * FROM bibliography").all())
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log("Up!")
+})
