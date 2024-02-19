7426357b9c695fa5a3ce2ff72f185f09e600b9f2
Trond Sneås Skauge <troska@asvg.no>
2024-02-19 11:33:15 +0100

    Noen rettelser i spørringene

--- app.js
+++ app.js
@@ -16,29 +16,29 @@ app.get("/forfattere", (req, res) => {
     res.json(db.prepare("SELECT * FROM author").all())
 })
 
-// Gir deg all info om en forfatter. Id refererer til forfatterens id i author-tabellen.
+// Gir deg all info om en forfatter. Id refererer til forfatterens id i author-tabellen. 
 app.get("/forfattere/:id", (req, res) => {
     res.json(db.prepare("SELECT * FROM author WHERE id = ?").get(req.params.id))
 })
 
 // Gir deg alle bøkene en forfatter har skrevet. Id refererer til forfatterens id i author-tabellen.
-app.get("/forfattere/:id/boker", (req, res) => {
+app.get("/forfatterBoker/:id", (req, res) => {
     res.json(db.prepare("SELECT * FROM bibliography WHERE author = ?").all(req.params.id))
 })
 
 // Gir deg all info om en bok. Id refererer til bokens id i bibliography-tabellen.
 app.get("/boker/:id", (req, res) => {
-    res.json(db.prepare("SELECT name, author.author, img, pages, summary, country FROM bibliography INNER JOIN author ON author.id = bibliography.author  WHERE bibliography.id = ? ").get(req.params.id))
+    res.json(db.prepare("SELECT name, author.author, bibliography.img, pages, summary, country FROM bibliography INNER JOIN author ON author.id = bibliography.author WHERE bibliography.id = ? ").get(req.params.id))
 })
 
 // Gir deg alle blurbs om en gitt bok. Du får forfatter som har skrevet blurben, samt selve blurben. Id refererer til bokens id i bibliography-tabellen.
 app.get("/blurbs/:id", (req, res) => {
-    res.json(db.prepare("SELECT blurb, author FROM blurbs INNER JOIN author ON author.id = blurb.blurb_author WHERE book_id = ?").get(req.params.id))
+    res.json(db.prepare("SELECT blurb, author FROM blurbs INNER JOIN author ON author.id = blurbs.blurb_author WHERE book_id = ?").all(req.params.id))
 })
 
 // Gir deg alle sjangrene til en bok. Id refererer til bokens id i bibliography-tabellen.
 app.get("/sjangre/:id", (req, res) => {
-    res.json(db.prepare("SELECT genre.genre FROM genrebooks INNER JOIN genre ON genrebooks.genre = genre.id  WHERE book = ?").get(req.params.id))
+    res.json(db.prepare("SELECT genre.genre FROM genrebooks INNER JOIN genre ON genrebooks.genre = genre.id  WHERE book = ?").all(req.params.id))
 })
 
 // Gir deg all info om alle bøkene
